c201941235ed5217ba68988494a55eb61540e631
Lei Wu <lei.wu@helvar.com>
2023-11-05 11:07:31 +0200

    feat: implement initial version of API and lambda to create a note

---
+++ packages/functions/src/create.ts
@@ -0,0 +1,52 @@
+import AWS from "aws-sdk";
+import * as uuid from "uuid";
+import { APIGatewayProxyEvent } from "aws-lambda";
+
+import { Table } from "sst/node/table";
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+export async function main(event: APIGatewayProxyEvent) {
+  let data, params;
+
+  // Request body is passed in as a JSON encoded string in 'event.body'
+  if (event.body) {
+    data = JSON.parse(event.body);
+    params = {
+      TableName: Table.Notes.tableName,
+      Item: {
+        // The attributes of the item to be created
+        userId: "123", // The id of the author
+        noteId: uuid.v1(), // A unique uuid
+        content: data.content, // Parsed from request body
+        attachment: data.attachment, // Parsed from request body
+        createdAt: Date.now(), // Current Unix timestamp
+      },
+    };
+  } else {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ error: true }),
+    };
+  }
+
+  try {
+    await dynamoDb.put(params).promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(params.Item),
+    };
+  } catch (error) {
+    let message;
+    if (error instanceof Error) {
+      message = error.message;
+    } else {
+      message = String(error);
+    }
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: message }),
+    };
+  }
+}
